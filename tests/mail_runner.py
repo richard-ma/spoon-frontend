4fa944e580fac0ad87ed9b8c52f8bb9f1f318663
Richard Ma <richard.ma.19850509@gmail.com>
2021-03-23 21:49:53 +0800

    mail test runner

---
+++ tests/mail_runner.py
@@ -0,0 +1,14 @@
+from lib.mail import Mail
+from settings import *
+
+if __name__ == '__main__':
+    host = MAIL_HOST
+    port = MAIL_PORT
+    username = MAIL_USERNAME
+    password = MAIL_PASSWORD
+    receiver = username  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+    subject = '测试标题'
+    message = '测试内容'
+
+    server = Mail(host, port, username, password, ssl=True)
+    server.send(receiver, subject, message)
