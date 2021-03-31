02f5b8a3c03af79b86d6279daa089e0956020b49
Richard Ma <richard.ma.19850509@gmail.com>
2021-03-31 23:34:09 +0800

    add

---
+++ spoonfrontend/helper.py
@@ -0,0 +1,14 @@
+def get_one_column(column, s):
+    return [item[column] for item in s]
+
+if __name__ == "__main__":
+    from datetime import date
+    from settings import *
+    from spoonfrontend.database import *
+    collection_name = 'top_summary'
+    client = Database.get_database_from_factory('mongodb')
+    client.connect(MONGO_URI, MONGO_DATABASE).set_collection(collection_name)
+
+    column = 'last_update'
+    items = get_one_column('summary', client.get_date(column, date(2021, 3, 23)))
+    print(items)
